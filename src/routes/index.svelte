ca49a7efb07f7ea13d285957932153ef90a278b7
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-05-20 13:11:45 -0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	const getJson = async (shouldHaveBody: boolean) => {
+		const resp = await fetch('/my-standalone-endpoint.json?which=json', {
+			method: 'POST',
+			body: shouldHaveBody ? JSON.stringify({ name: 'Elliott' }) : undefined
+		});
+		return (await resp.json()) as { name: string };
+	};
+
+	const getFormData = async (shouldHaveBody: boolean) => {
+		const potentialBody = new FormData();
+		potentialBody.append('name', 'Elliott');
+
+		const resp = await fetch('/my-standalone-endpoint.json?which=formData', {
+			method: 'POST',
+			body: shouldHaveBody ? potentialBody : undefined
+		});
+		return (await resp.json()) as { name: string };
+	};
+
+	let promise = Promise.resolve({ name: 'world' });
+</script>
+
+<button on:click={() => (promise = getJson(false))}>Get JSON with no body</button>
+<button on:click={() => (promise = getJson(true))}>Get JSON with body</button>
+<button on:click={() => (promise = getFormData(false))}>Get formData with no body</button>
+<button on:click={() => (promise = getFormData(true))}>Get formData with body</button>
+
+{#await promise}
+	<p>Loading...</p>
+{:then result}
+	<p>{JSON.stringify(result)}</p>
+{:catch error}
+	<p>{JSON.stringify(error)}</p>
+{/await}
---
+++ src/routes/my-standalone-endpoint.json.ts
@@ -0,0 +1,30 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const post: RequestHandler = async ({ request, url }) => {
+  const which = url.searchParams.get('which') as 'json' | 'formData';
+
+  let body;
+  switch (which) {
+    case 'json':
+      try {
+        body = await request.json()
+      } catch (e) {
+        body = { error: (e as Error)?.message };
+      }
+      return {
+        body
+      }
+    case 'formData':
+      try {
+        const formData = await request.formData();
+        body = { name: formData.get('name')?.toString() ?? 'world' }
+      } catch (e) {
+        body = { error: (e as Error)?.message };
+      }
+      return {
+        body
+      }
+    default:
+      throw new Error("What're you doing, you coward?")
+  }
+}
